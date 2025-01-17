f0f0f32aa723b10b2948f705ded6339c3f4daf04
ajiles91 <ajiles91@gmail.com>
2025-01-16 18:24:40 -0600

    working w database conn

--- src/database.ts
+++ src/database.ts
@@ -1,23 +1,3 @@
-// import { Database } from "./types"; // this is the Database interface we defined earlier
-// import { Pool } from "pg";
-// import { Kysely, PostgresDialect } from "kysely";
-// import "dotenv/config";
-
-// const dialect = new PostgresDialect({
-//   pool: new Pool({
-//     connectionString: process.env.POSTGRES_URL,
-//     database: process.env.PGDATABASE,
-//     host: process.env.PGHOST,
-//     user: process.env.PGUSER,
-//     // port: 5434,
-//     max: 50,
-//   }),
-// });
-
-// Database interface is passed to Kysely's constructor, and from now on, Kysely
-// knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
-// to communicate with your database.
 // export const db = new Kysely<Database>({
 //   dialect,
 // });
