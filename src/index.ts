a855702d9fd563ed7013a7d3cf6285ea065ac354
ajiles91 <ajiles91@gmail.com>
2025-03-03 21:14:15 -0600

    missed indexTS

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import Routes from "./routes";
 export default class Server {
   constructor(app: Application) {
     this.config(app);
-    new Routes(app);
+    // new Routes(app);
   }
   private config(app: Application): void {
     const corsOptions: CorsOptions = {
